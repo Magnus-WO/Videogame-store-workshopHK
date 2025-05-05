a0af592571dff692f02f2e597ef79c5ee6b73d2c
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2025-05-05 10:16:34 +0200

    add home-page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,24 @@
 import styles from "./Home.module.css";
-
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <section>Home Page</section>;
+  return (
+    <section className={styles.hero}>
+      <header className={styles.heroContent}>
+        <h1 className={styles.heroTitle}>Games Horizon</h1>
+        <p className={styles.heroSubtitle}>
+          Games Horizon - Where Every Adventure Begins. Your Portal to Infinite
+          Worlds. Play Beyond Limit. Play Beyond the Horizon
+        </p>
+        <Link
+          to="/games"
+          className={styles.ctaButton}
+          aria-label="explore available games"
+        >
+          Explore Games
+        </Link>
+      </header>
+    </section>
+  );
 };
 
 export default Home;
